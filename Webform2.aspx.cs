ee56b91b19c57f119b6ba7d2e543fa9e322a4e58
sanikaghule16 <sanikaghule4@gmail.com>
2026-03-15 21:40:08 +0530

    Create Webform2.aspx.cs

---
+++ Webform2.aspx.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Web.UI;
+
+namespace Registration_Form
+{
+    public partial class WebForm2 : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // No redirect needed; just display success message
+        }
+    }
+}
